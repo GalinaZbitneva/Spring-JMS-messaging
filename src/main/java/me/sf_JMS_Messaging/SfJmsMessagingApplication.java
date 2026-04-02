3b180e36fa8948349327abce1b8c4a97e0665ec3
Galina Zbitneva <galinaknurenko@gmail.com>
2026-04-02 14:16:52 +0300

    Initial commit

---
+++ src/main/java/me/sf_JMS_Messaging/SfJmsMessagingApplication.java
@@ -0,0 +1,13 @@
+package me.sf_JMS_Messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfJmsMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfJmsMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sf_JMS_Messaging/SfJmsMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sf_JMS_Messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfJmsMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
