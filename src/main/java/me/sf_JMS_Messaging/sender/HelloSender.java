f872d09430629c937148519d88898a5f5549da9d
Galina Zbitneva <galinaknurenko@gmail.com>
2026-04-09 13:06:23 +0300

    added message sender

---
+++ src/main/java/me/sf_JMS_Messaging/sender/HelloSender.java
@@ -0,0 +1,40 @@
+package me.sf_JMS_Messaging.sender;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import me.sf_JMS_Messaging.config.JmsConfig;
+import me.sf_JMS_Messaging.model.HelloWorldMessage;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    //fixedRate = 2000 ml seconds   это 2 секунды
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+
+        //помеченные  @Scheduled подхватываются  TaskExecutor в TaskConfig
+        System.out.println("I'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("World! World! World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE,message);
+
+        System.out.println("Message sent!!!");
+
+
+
+    }
+
+}
